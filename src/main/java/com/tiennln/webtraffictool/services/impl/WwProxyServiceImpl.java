d173f8d812d7bfbbab8c60855aad92b8b530d1a4
Theon <thinhtpt.2000@gmail.com>
2024-04-20 15:57:38 +0700

    feat: updated `WwProxyServiceImpl` to handle error

--- src/main/java/com/tiennln/webtraffictool/services/impl/WwProxyServiceImpl.java
+++ src/main/java/com/tiennln/webtraffictool/services/impl/WwProxyServiceImpl.java
@@ -1,8 +1,12 @@
 package com.tiennln.webtraffictool.services.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tiennln.webtraffictool.clients.WwProxyClient;
 import com.tiennln.webtraffictool.helpers.ThreadHelper;
+import com.tiennln.webtraffictool.models.WwProxyModel;
 import com.tiennln.webtraffictool.services.WwProxyService;
+import feign.FeignException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,8 +34,10 @@ public class WwProxyServiceImpl implements WwProxyService {
                     proxy = proxyData.getData().getProxy().trim();
                     isSuccess = true;
                 }
-                if (!isSuccess && tooManyRequestErrorCodes.contains(proxyData.getErrorCode())) {
-                    ThreadHelper.waitInMs(60L * 1000);
+            } catch (FeignException ex) {
+                log.error("Caught FeignException when WwProxyService.getProxy", ex);
+                if (ex instanceof FeignException.BadRequest badRequestEx) {
+                    processBadRequestException(badRequestEx);
                 }
             } catch (Exception ex) {
                 log.error("Error when WwProxyService.getProxy", ex);
@@ -39,5 +45,17 @@ public class WwProxyServiceImpl implements WwProxyService {
         } while (!isSuccess);
 
         return proxy;
+    }
+
+    private void processBadRequestException(FeignException.BadRequest badRequestEx) {
+        try {
+            var response = new ObjectMapper().readValue(badRequestEx.contentUTF8(), WwProxyModel.class);
+            if (tooManyRequestErrorCodes.contains(response.getErrorCode())) {
+                log.info("Found too many request, wait for 60s");
+                ThreadHelper.waitInMs(60L * 1000);
+            }
+        } catch (JsonProcessingException ex) {
+            log.error("Error when WwProxyService.processBadRequestException", ex);
+        }
     }
 }
