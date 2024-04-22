2dec1dbcd5bab98aa01144a2e4967fec7575d6ed
unknown <TienNLN>
2024-04-22 21:42:06 +0700

    Apply thread pool

--- src/main/java/com/tiennln/webtraffictool/controllers/ProcessController.java
+++ src/main/java/com/tiennln/webtraffictool/controllers/ProcessController.java
@@ -2,6 +2,7 @@ package com.tiennln.webtraffictool.controllers;
 
 import com.tiennln.webtraffictool.handlers.ProcessHandler;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,16 @@ public class ProcessController {
 
     @PostMapping
     void process() {
-        new Thread(() -> handler.start()).start();
+        handler.startWithThreadPool();
+    }
+
+    @PostMapping("/terminate")
+    void terminate() {
+        handler.terminateThreadPool();
+    }
+
+    @GetMapping("/processed-thread")
+    Integer getProcessedThread() {
+        return handler.getProcessedThread();
     }
 }
